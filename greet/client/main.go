52fb3c8248868e3ee8220b614dc088548ae633af
Midhun Jithu <midhun.jithu@ibm.com>
2025-12-30 16:05:12 +0530

    basic client conection

---
+++ greet/client/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var address = "localhost:50051"
+
+func main() {
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect to grpc server %v", err)
+	}
+	log.Print("connected to grpc server")
+	defer conn.Close()
+}
